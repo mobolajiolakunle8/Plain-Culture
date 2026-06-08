97be67772d5796c0a3560dbb6295d0f7bb1b83ba
mobolajiolakunle8 <mobolajiolakunle8@gmail.com>
2026-06-08 20:06:06 +0100

    Add files via upload

--- src/components/CartAndCheckoutDrawer.tsx
+++ src/components/CartAndCheckoutDrawer.tsx
@@ -495,9 +495,7 @@ export const CartAndCheckoutDrawer: React.FC<CartAndCheckoutDrawerProps> = ({
                 {/* Delivery Zone Selector */}
                 <div>
                   <label className="block text-xs font-black uppercase tracking-wider text-zinc-700 dark:text-zinc-300 mb-1.5">
-                    Delivery Location / Landmark 
-                    <p className="text-[10px] text-zinc-400 mt-1">Kindly note that your order will be delivered to the landmark specified during the checkout process.
-                      )*
+                    Delivery Location / Landmark *
                   </label>
                   <select
                     value={formData.deliveryZoneId}
